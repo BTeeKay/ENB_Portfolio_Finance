888bc1f1aa93ee891bb21e992b9402130284fd6f
Brian Kerr <briantkerr91@gmail.com>
2022-07-01 15:27:46 +0100

    This was an accident

--- enb_portfolio_finance/src/components/ShareBox.js
+++ enb_portfolio_finance/src/components/ShareBox.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 const ShareBox = () => {
 
+    const options = {
+        title: {
+          text: 'My stock chart'
+        },
+        series: [
+          {
+            data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9]
+          }
+        ]
+      };
+
     return (
         <div className="sharebox">
-            <h2>
-                This is ShareBox
-            </h2>
+            <HighchartsReact
+            highcharts={Highcharts}
+            constructorType={'stockChart'}
+            options={options}
+            />
         </div>
     )
 }
