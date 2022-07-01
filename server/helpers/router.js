7d855831eab429cb953925eebd6103e66eb31fab
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-07-01 13:48:56 +0100

    Added create route to router.js

--- server/helpers/router.js
+++ server/helpers/router.js
@@ -17,12 +17,28 @@ const createRouter = function (collection) {
                 res.json({ status: 500, error: err });
             });
     });
+
     // Show route
     router.get('/:id', (req, res) => {
         const id = req.params.id;
         collection
             .findOne({ _id: ObjectId(id) })
             .then((doc) => res.json(doc))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err });
+            });
+    });
+
+    // Create Route
+    router.post('/', (req, res) => {
+        const newData = req.body;
+        collection
+            .insertOne(newData)
+            .then((result) => {
+                res.json(result)
+            })
             .catch((err) => {
                 console.error(err);
                 res.status(500);
