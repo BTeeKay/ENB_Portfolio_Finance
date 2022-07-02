818d590e17a31c75345d428335ad8140997e61a2
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-07-02 17:48:18 +0100

    astarted work on horizontal portfolio menu

--- enb_portfolio_finance/src/components/PortfolioListItem.js
+++ enb_portfolio_finance/src/components/PortfolioListItem.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useContext } from 'react';
+import { VisibilityContext } from "react-horizontal-scrolling-menu";
 
 const PortfolioListItem = ({ share }) => {
-
+    // const visibility = useContext(VisibilityContext);
 
 
     return (
@@ -9,6 +11,7 @@ const PortfolioListItem = ({ share }) => {
             <h2>{share["Meta Data"]["2. Symbol"]}</h2>
             <ul>
                 <li>Units Held: {share["Meta Data"]["8. Units Held"]}</li>
+                <li>Bought Price: {share["Meta Data"]["6. Purchased Price"]}</li>
                 <li>Current Price Goes Here</li>
                 <li>Total Value Goes Here</li>
             </ul>
