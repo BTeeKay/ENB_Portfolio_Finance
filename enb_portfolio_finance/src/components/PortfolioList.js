80e67dc997a3ebac95da144dbe2152f849de9333
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-07-02 13:08:08 +0100

    Adding all new files to git

---
+++ enb_portfolio_finance/src/components/PortfolioList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PortfolioList = ({ portfolio_shares }) => {
+    const portfolioNodes = portfolio_shares.map(share => {
+        return <PortfolioListItem
+            key={share._id}
+            share={share}
+        />
+    });
+
+    return (
+        <div className='portfolio'>
+            <h3>Portfolio</h3>
+            {portfolioNodes}
+        </div>
+    )
+};
+
+export default PortfolioList;
