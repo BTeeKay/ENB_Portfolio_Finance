d9483ac3b82ed516213b58e85ef8621753fa9efc
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-07-02 12:57:15 +0100

    Added PortfolioService file and added database fetch functions for get, add and update

---
+++ enb_portfolio_finance/src/services/PortfolioService.js
@@ -0,0 +1,30 @@
+const baseURL = 'http://localhost:9000/api/shares';
+
+const PortfolioService = {
+
+    getPortfolioShares() {
+        return fetch(baseURL)
+            .then(res => res.json());
+    },
+
+    addPortfolioShares(share) {
+        return fetch(baseURL, {
+            method: 'POST',
+            body: JSON.stringify(share),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json());
+    },
+
+    updatePortfolioShares(share) {
+        return fetch(baseURL + share._id, {
+            method: 'PUT',
+            body: JSON.stringify(share),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+    }
+}
