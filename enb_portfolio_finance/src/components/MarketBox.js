6a82ed2ef92f0da3d59e588b14e6fa2deaa0f30d
Nick Jones <nickj_24@hotmail.com>
2022-07-02 19:05:04 +0100

    market_shares_components completed

---
+++ enb_portfolio_finance/src/components/MarketBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import MarketDetail from './MarketDetail';
+
+const MarketBox = ({marketShare, stockNameFromSearch, getStockData}) => {
+
+    const handleChange = (event) => {
+        const inputText = event.target.value
+        if (inputText.length === 4) {
+            stockNameFromSearch(inputText)
+            getStockData(inputText)
+        }
+    }
+
+    return (
+        <>
+        <h2> Market Shares</h2>
+        <div>
+            <h4>Search a stock</h4>
+            <form>
+                <input maxLength="4" className="searchShares"
+                onChange={handleChange} placeholder="Enter stock symbol"
+                type="text"></input>
+            </form>
+        </div>
+        <MarketDetail stock={marketShare}/>
+        </>
+
+    )
+}
+
+export default MarketBox;
