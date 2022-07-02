9a5e2a1cdef387e6554b75473549421f43dd12ee
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-07-02 15:50:57 +0100

    Portfolio can now be displayed on the page

--- enb_portfolio_finance/src/components/PortfolioListItem.js
+++ enb_portfolio_finance/src/components/PortfolioListItem.js
@@ -8,7 +8,7 @@ const PortfolioListItem = ({ share }) => {
         <div className="portfolio-share">
             <h2>{share["Meta Data"]["2. Symbol"]}</h2>
             <ul>
-                <li>{share["Meta Data"]["8. Units Held"]}</li>
+                <li>Units Held: {share["Meta Data"]["8. Units Held"]}</li>
                 <li>Current Price Goes Here</li>
                 <li>Total Value Goes Here</li>
             </ul>
