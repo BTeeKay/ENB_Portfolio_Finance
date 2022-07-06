15b381fe166d40cae9fe69c54b9d86179c16bddb
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2022-07-06 11:16:43 +0100

    updated CSS

--- enb_portfolio_finance/src/components/PortfolioListItem.js
+++ enb_portfolio_finance/src/components/PortfolioListItem.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 
 const PortfolioListItem = ({ share, onClick, }) => {
@@ -9,9 +9,9 @@ const PortfolioListItem = ({ share, onClick, }) => {
     const [currentPrice, setCurrentPrice] = useState("");
 
     fetch(`https://cloud.iexapis.com/stable/stock/${share.symbol}/quote?token=${API_KEY}`)
-            .then(result => result.json())
-            .then(price => setCurrentPrice(price["latestPrice"]))
-            share.currentPrice = currentPrice
+        .then(result => result.json())
+        .then(price => setCurrentPrice(price["latestPrice"]))
+    share.currentPrice = currentPrice
 
     const totalBoughtValue = share["Units Held"] * share.latestPrice;
 
@@ -20,6 +20,23 @@ const PortfolioListItem = ({ share, onClick, }) => {
 
     const handleClick = () => {
         onClick(share)
+    }
+
+    if (totalCurrentPrice > totalBoughtValue) {
+        return (
+            <div className="portfolio-share">
+                <h2 onClick={handleClick}>{share.symbol}</h2>
+                <ul>
+                    <li>Units Held: {share["Units Held"]}</li>
+                    <li>Bought Price: {share.latestPrice}</li>
+                    <li>Current Price: {share.currentPrice}</li>
+                    <li>Total Price When Bought: {totalBoughtValue}</li>
+                    <div className="green">
+                        <li>Total Current Price: {totalCurrentPrice}</li>
+                    </div>
+                </ul>
+            </div>
+        )
     }
 
 
@@ -31,7 +48,9 @@ const PortfolioListItem = ({ share, onClick, }) => {
                 <li>Bought Price: {share.latestPrice}</li>
                 <li>Current Price: {share.currentPrice}</li>
                 <li>Total Price When Bought: {totalBoughtValue}</li>
-                <li>Total Current Price: {totalCurrentPrice}</li>
+                <div className="red">
+                    <li>Total Current Price: {totalCurrentPrice}</li>
+                </div>
             </ul>
         </div>
     )
